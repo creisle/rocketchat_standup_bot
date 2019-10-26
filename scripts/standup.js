87a9f2ef9e72168465916751cf3a86effe409872
Caralyn Reisle <creisle@bcgsc.ca>
2019-10-25 17:20:50 -0700

    remove leftoever template comments

--- scripts/standup.js
+++ scripts/standup.js
@@ -1,11 +1,7 @@
 // Description:
-//    An example script, tells you the time. See below on how documentation works.
 //    https://github.com/hubotio/hubot/blob/master/docs/scripting.md#documenting-scripts
 //
-// Commands:
-//    bot what time is it? - Tells you the time
-//    bot what's the time? - Tells you the time
-//
+
 const Conversation = require('hubot-conversation');
 const schedule = require('node-schedule');
 const fs = require('fs');
